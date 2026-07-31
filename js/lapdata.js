4e26f57d0be5c6cfce180d97ff58c7767f08db2e
Angus <adlaird6471@gmail.com>
2026-07-31 13:16:24 -0700

    updated currrent race and track info in laps data
    coo

--- js/lapdata.js
+++ js/lapdata.js
@@ -53,6 +53,31 @@ export async function setlaps(){
     }
 }
 
+export async function getseshdata(){
+    const url = "https://api.openf1.org/v1/sessions?session_key=latest";
+    if(cachecheck()|| !localStorage.getItem("track")){
+    const data = await fetchData(url);
+    if (data){
+        const trac = document.getElementById('track');
+        const loca = document.getElementById('loc');
+        const sta = document.getElementById('stage');
+        trac.textContent = 'Track: ' + data[0].circuit_short_name;
+        loca.textContent = 'Location: ' + data[0].country_name;
+        sta.textContent = 'Stage: ' + data[0].session_name;
+
+        localStorage.setItem("track", data[0].circuit_short_name);
+        localStorage.setItem("loc", data[0].country_name);
+        localStorage.setItem("stage", data[0].session_name);
+    }
+}
+        const trac = document.getElementById('track');
+        const loca = document.getElementById('loc');
+        const sta = document.getElementById('stage');
+        trac.textContent = 'Track: ' + localStorage.getItem("track");
+        loca.textContent = 'Location: ' + localStorage.getItem("loc");
+        sta.textContent = 'Stage: ' + localStorage.getItem("stage");
+}
+
 
 
     
